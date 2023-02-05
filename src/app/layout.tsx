197903b0df751b04c4ebb8702f9126fbd388d45b
Abshir <abshir.y.ahmed@gmail.com>
2023-02-05 22:13:07 +0000

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,13 @@
 import './globals.css';
 import { ReactNode } from 'react';
+import { AnalyticsWrapper } from '@/app/components/analytics';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head />
       <body>{children}</body>
+      <AnalyticsWrapper />
     </html>
   );
 }
