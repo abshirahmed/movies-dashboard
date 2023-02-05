8e57fa6fefa17e9c2410e82709eb9ebb6bbf58cd
Abshir <abshir.y.ahmed@gmail.com>
2023-02-05 14:25:15 +0000

    init tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
-import { Inter } from '@next/font/google';
-import styles from './page.module.css';
-
-const inter = Inter({ subsets: ['latin'] });
-
 export default async function Home() {
   return (
-    <main className={styles.main}>
-      <h1 className={styles.card}>Movies Dashboard</h1>
+    <main>
+      <h1>Movies Dashboard</h1>
     </main>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    }
+  },
+  plugins: []
+};
