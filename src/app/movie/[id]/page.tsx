652e86a820e77aa9e49326431e067c3118a7cdcf
Abshir <abshir.y.ahmed@gmail.com>
2023-02-06 13:29:44 +0000

    fetch data on movie page

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,3 +1,14 @@
-export default async function MoviePage() {
-  return <h1>Movie by id page</h1>;
+export const runtime = 'experimental-edge'; // 'nodejs' (default) | 'experimental-edge'
+
+import { fetcher } from '@/lib/fetcher';
+import { MovieResponse } from '@/pages/api/movie/validators';
+
+export default async function MoviePage({ params }: { params: { id: string } }) {
+  const { Title } = await fetcher<MovieResponse>(`/api/movie?i=${params.id}`);
+
+  return (
+    <main>
+      <h1>Title: {Title}</h1>
+    </main>
+  );
 }
