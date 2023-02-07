0d1b4f27a4ddcd97fd7bdf18d44b89cef7e54b7e
Abshir <abshir.y.ahmed@gmail.com>
2023-02-07 09:47:18 +0000

    improve movie card

--- src/app/components/MovieCard.tsx
+++ src/app/components/MovieCard.tsx
@@ -2,25 +2,41 @@ import { MovieResponse } from '@/pages/api/movie/validators';
 import Image from 'next/image';
 import Link from 'next/link';
 
-type MovieCardProps = Pick<MovieResponse, 'imdbID' | 'Title' | 'Plot' | 'Poster'>;
+export default function MovieCard(props: MovieResponse) {
+  const genres = props?.Genre?.split(', ');
 
-export default function MovieCard(props: MovieCardProps) {
   return (
-    <Link
-      href={`/movie/${props.imdbID}`}
-      className="flex flex-col items-center rounded-lg border border-gray-200 bg-white shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 md:max-w-xl md:flex-row"
-    >
-      <Image
-        className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-        src={props.Poster!}
-        alt=""
-        width={200}
-        height={300}
-      />
-      <div className="flex flex-col justify-between p-4 leading-normal">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{props.Title}</h5>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{props.Plot}</p>
+    <div className="flex flex-col">
+      <div className="py-3 sm:mx-auto sm:max-w-xl">
+        <div className="flex max-h-80 space-x-8 rounded-3xl border bg-white p-8 shadow-lg">
+          <div className="w-1/2 overflow-visible">
+            <Image className="rounded-3xl shadow-lg" src={props?.Poster!} alt="" width={300} height={450} />
+          </div>
+          <div className="flex w-1/2 flex-col space-y-4">
+            <div className="flex items-start justify-between">
+              <Link href={`https://imdb.com/title/${props.imdbID}`} className="hover:underline" target="_blank">
+                <h2 className="text-3xl font-bold">{props.Title}</h2>
+              </Link>
+              <div className="rounded-xl bg-yellow-400 p-2 font-bold">{props.imdbRating}</div>
+            </div>
+            <div className="hidden md:block">
+              <div className="text-sm capitalize text-gray-400">{props.Type}</div>
+              <div className="text-lg text-gray-800">{props.Year}</div>
+            </div>
+            <p className=" max-h-40 overflow-auto text-gray-400">{props.Plot}</p>
+            <div className="hidden flex-wrap gap-2 sm:flex">
+              {genres?.map((actor, index) => (
+                <div
+                  key={index}
+                  className="inline-flex items-center rounded-full bg-gray-100 px-2.5 py-0.5 text-xs font-medium text-gray-800"
+                >
+                  {actor}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
-    </Link>
+    </div>
   );
 }
